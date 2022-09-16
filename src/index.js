2b5f381dc3f2dc1b15efcb0a7062834417ec2280
Debolu <obayemiadebolu16@gmail.com>
2022-09-16 23:38:43 +0100

    Practice excercise for React.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Great from './App';
+// import { useState } from 'react';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>
+//         Increment
+//       </button>
+//     </>
+//   );
+// }
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Great name='Adebolu' title='Obayemi' />
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
